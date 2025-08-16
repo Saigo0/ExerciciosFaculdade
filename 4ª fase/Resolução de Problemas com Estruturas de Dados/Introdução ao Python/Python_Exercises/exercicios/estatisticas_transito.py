c0ab139132642ef2bd67367a7b234686e5536bc8
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-08-15 21:17:21 -0300

    atualização exercícios

--- 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/estatisticas_transito.py
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/estatisticas_transito.py
@@ -1,29 +1,35 @@
 from xmlrpc.client import MAXINT
 
-city1cod = int(input("Insira o código da cidade 1: "))
-city1vehicles = int(input("Insira quantos veículos a cidade 1 tem: "))
-city1crashes = int(input("Insira o número de acidentes de trânsito com vítimas na cidade 1: "))
-
-city2cod = int(input("Insira o código da cidade 2: "))
-city2vehicles = int(input("Insira quantos veículos a cidade 2 tem: "))
-city2crashes = int(input("Insira o número de acidentes de trânsito com vítimas na cidade 2: "))
-
-city3cod = int(input("Insira o código da cidade 3: "))
-city3vehicles = int(input("Insira quantos veículos a cidade 3 tem: "))
-city3crashes = int(input("Insira o número de acidentes de trânsito com vítimas na cidade 3: "))
-
-city4cod = int(input("Insira o código da cidade 4: "))
-city4vehicles = int(input("Insira quantos veículos a cidade 4 tem: "))
-city4crashes = int(input("Insira o número de acidentes de trânsito com vítimas na cidade 4: "))
-
-city5cod = int(input("Insira o código da cidade 5: "))
-city5vehicles = int(input("Insira quantos veículos a cidade 5 tem: "))
-city5crashes = int(input("Insira o número de acidentes de trânsito com vítimas na cidade 5: "))
 
 maiorIndice = 0
 maiorIndiceCod = ""
 menorIndice = MAXINT
 menorIndiceCod = ""
-
+media = 0
+mediaMq2000 = 0
+cont = 0
 for i in range (1, 6):
-    
+
+    cod = input("Digite o codigo da cidade " )
+    numV = int(input("Digite o numero de veículos de passeio da cidade " ))
+    numA = int(input("Digite a quantia de acidentes de trânsito com vítimas: "))
+    if numA > maiorIndice:
+        maiorIndice = numA
+        maiorIndiceCod = cod
+
+    if numA < menorIndice:
+        menorIndice = numA
+        menorIndiceCod = cod
+
+    media += numV
+    if numA < 2000:
+        mediaMq2000 += numA
+        cont += 1
+
+media = media/5
+mediaMq2000 = mediaMq2000/cont
+
+print("Maior índice de acidentes: ", maiorIndice, ", código da cidade: ", maiorIndiceCod)
+print("Menor índice de acidentes: ", menorIndice, ", código da cidade: ", menorIndiceCod)
+print("Média de veículos nas 5 cidades: ", media)
+print("Média de acidentes nas cidades com menos de 2000 veículos", mediaMq2000)
---
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/fibonacci.py
@@ -0,0 +1,5 @@
+n = int(input("Quantidade de termos na sequência: "))
+seq = 1
+aux = 0
+
+for i in range(1, n+1):
