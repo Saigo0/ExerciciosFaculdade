0c971879d9d31363608b0264d0f7e21ba8023ef8
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-08-20 18:20:40 -0300

    commit exercícios python

---
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/brinquedos.py
@@ -0,0 +1,7 @@
+a = 0
+b = 0
+c = 0
+while a >= 0 and b >= 0 and c >= 0:
+    a = float(input("Digite o valor do comprimento da caixa: "))
+    b = float(input("Digite o valor da largura da caixa: "))
+    c = float(input("Digite o valor da altura da altura: "))
