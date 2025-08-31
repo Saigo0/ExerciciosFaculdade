4413bfa22fac1c462cf22a04d80a79741d89def3
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-08-31 00:28:19 -0300

    commit ajuste de exercicios

--- 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/apresenta_numeros.py
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/apresenta_numeros.py
@@ -4,43 +4,43 @@ B = int(input("Insira o valor de B: "))
 C = int(input("Insira o valor de C: "))
 
 if I == 1:
-    if C > B and B > A:
+    if C > B > A:
         print(A,B,C)
-    elif B > C and C > A:
+    elif B > C > A:
         print(A,C,B)
-    elif A > B and C > A:
+    elif B < A < C:
         print(B, A, C)
     elif A > B and C > B and A > B:
         print(B,C,A)
-    elif B > C and A > C and B > A:
+    elif B > C and C < A < B:
         print(C,A,B)
     else:
         print(C,B,A)
 
 if I == 2:
-    if A > B and B > C:
+    if A > B > C:
         print(A,B,C)
-    elif B > C and C > A:
+    elif B > C > A:
         print(A,C,B)
-    elif B > C and C > A:
+    elif B > C > A:
         print(B,C,A)
-    elif A > C and B > A:
+    elif C < A < B:
         print(B,A,C)
-    elif C > B and B > A:
+    elif C > B > A:
         print(C,B,A)
     else:
         print(C,A,B)
 
 if I == 3:
-    if B > C and C > A and B > A:
+    if B > C > A and B > A:
         print(A,B,C)
-    elif C > B and C > A and B > A:
+    elif C > B > A and C > A:
         print(A,C,B)
-    elif C > B and C > A and A > B:
+    elif C > B and C > A > B:
         print(B,C,A)
-    elif A > B and A > C and C > B:
+    elif A > B and A > C > B:
         print(B,A,C)
-    elif B > C and A > C and B > A:
+    elif B > C and C < A < B:
         print(C,B,A)
     else:
         print(C,A,B)
--- 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/repeticao.py
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/repeticao.py
@@ -1,10 +1,42 @@
 lista = [0] * 5
-
-for i in range(5):
-    lista[i] = i
+lista[0] = 1
+lista[1] = 2
+lista[2] = 2
+lista[3] = 3
+lista[4] = 4
 
 cont = [0] * 5
 valores = [0] * 5
 
 for i in range (5):
-    
+    for j in range(5):
+        if lista[i] == lista[j]:
+            valores[i] = lista[i]
+            cont[i] += 1
+
+for i in range (5):
+    if cont[i] == 1:
+        cont[i] = 0
+        valores[i] = 0
+
+cont2 = 0
+
+for i in range (5):
+    if cont[i] == 0:
+        cont2 += 1
+
+lista2 = [0] * (5 - cont2)
+
+for i in range (len(lista2)):
+    for j in range(5):
+        if valores[j] != 0:
+            lista2[i] = valores[j]
+
+for i in range (len(lista2)):
+    for j in range(len(lista2)):
+        if lista2[i] == lista2[j]:
+
+
+
+print("Valores que se repetem: ", lista2)
+print("Vezes que se repetem: ", cont)
