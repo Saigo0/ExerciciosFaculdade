c893ebb30297e5448e40e5c766c7dc9a489b7e10
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-09-01 17:56:10 -0300

    commit exercicio repeticao

--- 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/repeticao.py
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Introdução ao Python/Python_Exercises/exercicios/repeticao.py
@@ -5,38 +5,16 @@ lista[2] = 2
 lista[3] = 3
 lista[4] = 4
 
-cont = [0] * 5
-valores = [0] * 5
+verificados = [0] * 5
 
-for i in range (5):
+for i in range(5):
+    cont = 0
     for j in range(5):
+        if lista[i] != verificados[j]:
+            verificados [j] = lista[i]
         if lista[i] == lista[j]:
-            valores[i] = lista[i]
-            cont[i] += 1
+            cont += 1
+    if cont > 1:
+        cont -= 1
+        print("O número", lista[i], "é repetido", cont, "vezes")
 
-for i in range (5):
-    if cont[i] == 1:
-        cont[i] = 0
-        valores[i] = 0
-
-cont2 = 0
-
-for i in range (5):
-    if cont[i] == 0:
-        cont2 += 1
-
-lista2 = [0] * (5 - cont2)
-
-for i in range (len(lista2)):
-    for j in range(5):
-        if valores[j] != 0:
-            lista2[i] = valores[j]
-
-for i in range (len(lista2)):
-    for j in range(len(lista2)):
-        if lista2[i] == lista2[j]:
-
-
-
-print("Valores que se repetem: ", lista2)
-print("Vezes que se repetem: ", cont)
