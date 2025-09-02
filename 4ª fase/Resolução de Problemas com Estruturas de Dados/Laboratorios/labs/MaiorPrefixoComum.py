19e7c99f9839ac40ccd2261230c741cd3a789745
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-09-01 21:11:04 -0300

    Criação laboratorios

---
+++ 4ª fase/Resolução de Problemas com Estruturas de Dados/Laboratorios/labs/MaiorPrefixoComum.py
@@ -0,0 +1,10 @@
+def prefixo (lista):
+    prefix = ""
+    isprefix = True
+    for i in range(len(lista)):
+        string1 = lista[i]
+        for j in range(len(string1)):
+          prefix += string1[j]
+
+
+
